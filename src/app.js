8527a53f3869591e1a08d668be5e73c737ebca09
procopio420 <lucas.p@gec.inatel.br>
2020-04-07 02:27:30 -0300

    desafio cumprido

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,65 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const id = uuid();
+  const likes = 0;
+
+  const rep = { id, title, url, techs, likes };
+  repositories.push(rep);
+
+  return response.json(rep);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  if(request.body.likes) return response.status(400).json({error:"Cannot update likes manually"});
+  
+  const index = repositories.findIndex((r)=>r.id === id);
+
+  if(index<0) return response.status(400).json({error:"ID not found"});
+
+  const likes = repositories[index].likes;
+
+  const rep = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  };
+
+  repositories[index] = rep;
+
+  return response.json(rep);
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+
+  const index = repositories.findIndex(r=>r.id===id);
+  if(index<0) return res.status(400).json({error:"ID not found"});
+
+  repositories.splice(index, 1);
+
+  return res.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex(r=>r.id===id);
+  if(index<0) return response.status(400).json({error:"ID not found"});
+
+  repositories[index].likes++;
+
+  return response.status(204).json();
 });
 
 module.exports = app;
